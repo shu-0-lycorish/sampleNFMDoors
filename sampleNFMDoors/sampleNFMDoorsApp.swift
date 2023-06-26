b34cf4df4aa48f5d8262d5f4d88b7d3eca198891
shu-0-lycoris <shu.kwn0257b@gmail.com>
2023-06-26 23:04:58 +0900

    Initial Commit

---
+++ sampleNFMDoors/sampleNFMDoorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleNFMDoorsApp.swift
+//  sampleNFMDoors
+//
+//  Created by Shuhei Kawano on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct sampleNFMDoorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
